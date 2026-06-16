7e2ec51411babd8e13fc3e3b1c8c5f25a1abdd44
jyeah110-hue <j.yeah110@gmail.com>
2026-06-16 12:28:12 +0900

    커밋메세지

--- src/main/java/com/example/greenlibrary/admin/main/AdminController.java
+++ src/main/java/com/example/greenlibrary/admin/main/AdminController.java
@@ -1,4 +1,4 @@
-package com.example.greenlibrary.controller.admin;
+package com.example.greenlibrary.admin.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
